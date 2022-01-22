5cd8d88fb1923de6f303c6ac42b30b2b9960f609
ZakharenkoIV <zaharenkoiv93@gmail.com>
2022-01-22 18:36:10 +0300

    1. Spring boot repository [#2095]

---
+++ src/main/java/ru/job4j/forum/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package ru.job4j.forum.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import ru.job4j.forum.model.Post;
+
+import java.util.List;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+    @Query("FROM Post")
+    List<Post> getAllPosts();
+}
--- src/main/java/ru/job4j/forum/service/PostService.java
+++ src/main/java/ru/job4j/forum/service/PostService.java
@@ -2,14 +2,18 @@ package ru.job4j.forum.service;
 
 import org.springframework.stereotype.Service;
 import ru.job4j.forum.model.Post;
-import ru.job4j.forum.repository.mem.PostMem;
+import ru.job4j.forum.repository.PostRepository;
 
 import java.util.List;
 
 @Service
 public class PostService {
 
-    private final PostMem postStore = new PostMem();
+    private final PostRepository postStore;
+
+    public PostService(PostRepository postStore) {
+        this.postStore = postStore;
+    }
 
     public List<Post> getAllPosts() {
         return postStore.getAllPosts();
